eaaa7e2f05ce6b1d160a0d45c041b861bbb6f76e
V-Jagath <jagath9360@gmail.com>
2025-12-18 22:16:08 +0530

    Notes Updated

--- Back_End/update-phase/src/TASKS/DailyTasks/Day19Task.tsx
+++ Back_End/update-phase/src/TASKS/DailyTasks/Day19Task.tsx
@@ -9,6 +9,9 @@ const Day19Task = () => {
             <div className="text-white flex flex-col gap-5 p-5">
                 <Shop />
             </div>
+            <div className="bg-white">
+                <StarRating />
+            </div>
         </div>
     </>)
 };
@@ -18,19 +21,31 @@ export default Day19Task;
 export const Shop = () => {
 
     const [product, setProduct] = useState([
-        { product: "Pen", price: "Rs.20", qty: 5 },
-        { product: "Pencil", price: "Rs.10", qty: 4 },
-        { product: "Scale", price: "Rs.15", qty: 3 },
-        { product: "Rubber", price: "Rs.5", qty: 2 },
+        { id: 1, product: "Pen", price: "Rs.20", qty: 5 },
+        { id: 2, product: "Pencil", price: "Rs.10", qty: 4 },
+        { id: 3, product: "Scale", price: "Rs.15", qty: 3 },
+        { id: 4, product: "Rubber", price: "Rs.5", qty: 2 },
     ]);
 
-    const IncreaseQty = () => {
-
+    const IncreaseQty = (i: number) => {
+        setProduct(prev =>
+            prev.map((item, i) => (
+                i === i ? { ...item, qty: item.qty + 1 } : item
+            ))
+        )
     };
 
     const DecreaseQty = () => {
 
-    };
+    }
+
+    // const increaseQty = (index: number) => {
+    //     setProduct(prev =>
+    //         prev.map((item, i) =>
+    //             i === index ? { ...item, qty: item.qty + 1 } : item
+    //         )
+    //     );
+    // }; i dont understand this code what is prev and what i and why this condition use 
 
     const getToalAmount = () => {
 
@@ -51,13 +66,41 @@ export const Shop = () => {
                     </div>
                 ))}
             </div>
-
-            <div className="flex flex-col gap-5 justify-center items-center">
-
-            </div>
         </div>
 
     </>)
 
 }
 
+export const StarRating = () => {
+    const [rating, setRating] = useState(0);
+
+    const value = [1, 2, 3, 4, 5]
+
+    const resetRating = () => {
+        setRating(0);
+    };
+
+    return (
+        <div style={{ fontSize: "30px" }}>
+            {value.map((star) => (
+                <span
+                    key={star}
+                    onClick={() => setRating(star)}
+                    style={{
+                        cursor: "pointer",
+                        color: star <= rating ? "gold" : "gray",
+                    }}
+                >
+                    ★
+                </span>
+            ))}
+
+            <div style={{ marginTop: "10px" }}>
+                <button onClick={resetRating}>Reset</button>
+            </div>
+
+            <p>Selected Rating: {rating}</p>
+        </div>
+    );
+};
