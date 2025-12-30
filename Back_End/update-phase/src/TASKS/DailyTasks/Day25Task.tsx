c2d0f145ad9de5f025efb5b3a219d02f52e24955
V-Jagath <jagath9360@gmail.com>
2025-12-30 13:25:45 +0530

    Task Completed

--- Back_End/update-phase/src/TASKS/DailyTasks/Day25Task.tsx
+++ Back_End/update-phase/src/TASKS/DailyTasks/Day25Task.tsx
@@ -20,14 +20,54 @@ const Day25Task = () => {
 
     return (
         <>
-            <form onClick={formData} >
+            <form onClick={formData}>
                 <input type="text" onChange={getVal} className="bg-red-200 text-black" />
                 <input type="submit" value="getData" />
                 <h1>{fdata}</h1>
             </form>
+            <OddEven />
         </>
     )
 }
 
 export default Day25Task;
 
+export const OddEven = () => {
+
+    const [data, setData] = useState(0)
+
+    const addNum = (e: any) => {
+
+        e.preventDefault()
+
+        const value = e.target.value
+
+        console.log(value)
+
+        if (Number(value) % 2 === 0) {
+            const random = Math.floor(Math.random() * 10) * 2
+            setData(random + value)
+        } else {
+            const random = Math.floor(Math.random() * 10)
+            setData(random + value)
+        }
+
+    }
+
+    // const getformData = (e: any) => {
+
+    //     e.preventDefault()
+
+    //     const formData = e.target.value
+
+    //     console.log(formData)
+    // }
+
+    return (<>
+        <form>
+            <input onChange={addNum} type="text" />
+            <input type="submit" />
+            <p>{data}</p>
+        </form>
+    </>)
+}
