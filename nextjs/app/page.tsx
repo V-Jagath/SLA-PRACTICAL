3d7c1a5e306a505877eeb70d9b2e060084005394
V-Jagath <jagath9360@gmail.com>
2026-01-04 11:50:15 +0530

    Task Updated

--- nextjs/app/page.tsx
+++ nextjs/app/page.tsx
@@ -1,10 +1,10 @@
+import Astryora from '@/app/components/astryora/page'
 
 export default function Home() {
   return (
-    <div className="bg-white dark:bg-black font-sans">
-      <main className="mx-auto max-w-7xl">
-        Hello its a Main page
-      </main>
+    <div>
+
+      <Astryora />
     </div>
   );
 }
