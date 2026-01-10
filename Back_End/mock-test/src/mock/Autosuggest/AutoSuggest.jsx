2617de5ac2cf64d8026148c667f04826f6a6e512
V-Jagath <jagath9360@gmail.com>
2026-01-10 20:24:09 +0530

    Auto Suggest task start

---
+++ Back_End/mock-test/src/mock/Autosuggest/AutoSuggest.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+
+const AutoSuggest = () => {
+
+    const [list, setList] = useState([
+        { fruits: ["apple", "orange", "banana"] },
+        { vegitables: ["onion", "tomoto", "pototo"] },
+        { flowers: ["jasmine", "rose", "lotus"] },
+    ]);
+
+    return (
+        <>
+
+        </>
+    )
+}
+
+export default AutoSuggest
