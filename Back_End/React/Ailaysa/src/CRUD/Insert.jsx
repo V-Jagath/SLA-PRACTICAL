d35fa9b072f2f677f134e9179316b167b795b9bc
V-Jagath <jagath9360@gmail.com>
2026-01-29 08:41:57 +0530

    Crud Operation done

--- Back_End/React/Ailaysa/src/CRUD/Insert.jsx
+++ Back_End/React/Ailaysa/src/CRUD/Insert.jsx
@@ -3,11 +3,10 @@ import { useEffect, useState } from "react"
 const Insert = () => {
 
     const [apiData, setApiData] = useState([])
-
-    const [formData, setFormData] = useState([
-        { title: "", price: "", category: "" },
-
-    ])
+    const [edit, setEdit] = useState(null)
+    const [formData, setFormData] = useState(
+        { title: "", price: "", category: "" }
+    )
     useEffect(() => {
         const fetchData = async () => {
             const res = await fetch('https://fakestoreapi.com/products')
@@ -15,7 +14,6 @@ const Insert = () => {
             setApiData(finalRes)
         }
         fetchData()
-
     }, [])
 
     const getInput = (e) => {
@@ -24,15 +22,47 @@ const Insert = () => {
     }
 
     const getFormData = (e) => {
+
         e.preventDefault()
 
-        const newProduct = { id: apiData.length + 1, ...formData }
 
-        setApiData([...apiData, formData])
+        if (edit) {
+            const updateData = apiData.map((item) => (item.id === edit ? { ...item, ...formData } : item
+            ))
+
+            setApiData(updateData)
+            setEdit(null)
+        } else {
+            const newProduct = { id: apiData.length + 1, ...formData }
+
+            setApiData([...apiData, newProduct])
+            setFormData({ title: "", price: "", category: "" })
+        }
+
+    }
+
+    //  Delete item
+
+    const deleteItem = (id) => {
+        const filterData = apiData.filter((e) => e.id !== id)
+        setApiData(filterData)
+    }
+
+    const catgory = [...new Set(apiData.map((item) => item.category))]
+
+    const showCategory = () => {
+        const showCategorylist = apiData.filter((e) => e.category === "men's clothing")
+        setApiData(showCategorylist)
+    }
+
+    const editItem = (item) => {
+        setFormData({ title: item.title, price: item.price, category: item.category })
+        setEdit(item.id)
     }
 
     return (
         <>
+
             <form onSubmit={getFormData}>
                 <label>Title</label>
                 <input type="text" onChange={getInput} name='title' />
@@ -42,15 +72,21 @@ const Insert = () => {
                 <input type="text" onChange={getInput} name='category' />
                 <input type="submit" value="Add Product" />
             </form>
+            {catgory.map((e, i) => (
+                <h1 key={i} >{e}</h1>
+            ))}
 
             {apiData.map((e) => (
-                <div key={apiData.length + 1}>
+                <div key={e.id}>
                     <p>id:{e.id}</p>
                     <p>Title : {e.title}</p>
                     <p>Price : {e.price}</p>
                     <p>Category : {e.category}</p>
+                    <button onClick={() => deleteItem(e.id)}>Delete</button>
+                    <button onClick={() => editItem(e.id)}>Edit</button>
                 </div>
             ))}
+            <button onClick={showCategory}>Show Jwellary</button>
         </>
     )
 }
